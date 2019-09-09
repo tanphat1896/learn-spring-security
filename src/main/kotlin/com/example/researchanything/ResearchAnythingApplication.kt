69f353b1ab9db77c7747093ebbc805673008ca08
Tan Phat <tanphat1896@gmail.com>
2019-09-09 19:59:55 +0700

    Init

---
+++ src/main/kotlin/com/example/researchanything/ResearchAnythingApplication.kt
@@ -0,0 +1,11 @@
+package com.example.researchanything
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ResearchAnythingApplication
+
+fun main(args: Array<String>) {
+	runApplication<ResearchAnythingApplication>(*args)
+}
