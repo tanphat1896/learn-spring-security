4748145215824b3c1d635ed446e0fcf6d60b6f9a
Tan Phat <tanphat1896@gmail.com>
2019-09-09 23:58:18 +0700

    Use h2 db

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,8 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	runtimeOnly("mysql:mysql-connector-java")
+	implementation("io.jsonwebtoken:jjwt:0.9.1")
+	runtimeOnly("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
 }
